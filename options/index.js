7b017ad9b5f9baa8539066466a75b70c919bd767
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-05-11 11:58:22 +0100

    added creds to options

---
+++ options/index.js
@@ -0,0 +1,15 @@
+const options = {
+  options: {
+    debug: true
+  },
+  connection: {
+    reconnect: true
+  },
+  id: {
+    username: 'drop_bot_dev',
+    pass: 'oauth:ls2f5wfpb1nycw5vsz0qy04nrx6lip'
+  },
+  channels: ['irradiatedunicorn']
+}
+
+module.exports = options;
